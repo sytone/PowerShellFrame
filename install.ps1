4eca3b7b9489fa7e224e429bb9f54a9cbd941cab
sytone <github@sytone.com>
2013-12-08 14:20:38 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,4 +4,11 @@
 # @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.github.com/sytone/PowerShellFrame/master/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\psf
 
 Write-Host "Welcome to the PowerShelFrame (PSF) "
-Write-Host "This is being installed at: %systemdrive%\psf"
+Write-Host "This is being installed at: $($env:systemdrive)\psf"
+
+if(-not (Test-Path "$($env:systemdrive)\psf")) {
+  Write-Host "Creating PSF"
+  New-Item "$($env:systemdrive)\psf" -Type Directory
+} else {
+  Write-Host "Upgrading PSF"
+}
