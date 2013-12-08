85ec26c2489e2f662e306d6cc3d2bfb90f4f0f94
sytone <github@sytone.com>
2013-12-08 14:17:56 -0800

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,7 @@
+# This will install and setup the local environment. 
+# Read through it first or just trust the code.. Your choice, your risk. 
+
+# @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.github.com/sytone/PowerShellFrame/master/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\psf
+
+Write-Host "Welcome to the PowerShelFrame (PSF) "
+Write-Host "This is being installed at: %systemdrive%\psf"
