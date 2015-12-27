68e4f9ed65e5dfd443e9e660df81728982694bcf
Sytone <sytone@users.noreply.github.com>
2015-12-26 17:09:09 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,12 +3,39 @@
 
 # @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.github.com/sytone/PowerShellFrame/master/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\psf
 
-Write-Host "Welcome to the PowerShelFrame (PSF) "
-Write-Host "This is being installed at: $($env:systemdrive)\psf"
+$psfRemoteRoot = "https://raw.github.com/sytone/PowerShellFrame/master"
+$psfLocalRoot =  Join-Path $env:USERPROFILE "psf"
+$psfLocalTemp = Join-Path $psfLocalRoot "temp"
+$psfLocalModules = Join-Path $psfLocalRoot "modules"
 
-if(-not (Test-Path "$($env:systemdrive)\psf")) {
+Write-Host "Welcome to the PowerShelFrame (PSF) "
+Write-Host "This is being installed at: $psfLocalRoot"
+
+if(-not (Test-Path $psfLocalRoot)) {
   Write-Host "Creating PSF"
-  New-Item "$($env:systemdrive)\psf" -Type Directory | Out-Null
+  New-Item $psfLocalRoot -Type Directory | Out-Null
+  New-Item $psfLocalTemp -Type Directory | Out-Null
+  New-Item $psfLocalModules -Type Directory | Out-Null 
+  Invoke-WebRequest "$psfRemoteRoot/localenv.ps1" -outfile "$psfLocalRoot\localenv.ps1"
 } else {
   Write-Host "Upgrading PSF"
 }
+
+Write-Host "Validating the profile $profile"
+$envLoadLine = "`n. $psfLocalRoot\localenv.ps1   #LOCALENV - May change in future`n"
+if ((Test-Path $profile) -eq $false) {
+  New-Item $profile -type file -force -ea 0 | Out-Null
+  $envLoadLine | Set-Content  ($profile)
+} else {
+  
+  (Get-Content ($profile)) | Foreach-Object {
+      $_ -replace '^.+#LOCALENV.+$', ($envLoadLine)
+    } | Set-Content  ($profile)
+    
+  $mi = Select-String -Path $profile -Pattern "#LOCALENV"
+  if(!$mi.Matches) { 
+    $profileData = (Get-Content ($profile)) 
+    ($profileData += $envLoadLine) | Set-Content  ($profile)
+  }
+}
+
