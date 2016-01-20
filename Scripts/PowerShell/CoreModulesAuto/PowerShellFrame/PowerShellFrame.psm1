a9dbaecad5b1d68ee0338f4444013a0887da4a6b
Sytone <github@sytone.com>
2016-01-20 08:27:26 -0800

    Lazy hack and commit

--- Scripts/PowerShell/CoreModulesAuto/PowerShellFrame/PowerShellFrame.psm1
+++ Scripts/PowerShell/CoreModulesAuto/PowerShellFrame/PowerShellFrame.psm1
@@ -113,13 +113,13 @@ function alias {
     $alias,$cmd = [string]::join(" ",$args).split("=",2) | % { $_.trim()}
     $cmd = Resolve-Aliases $cmd
     
-    New-Item -Path function: -Name "Global:Alias$Alias" -Options "AllScope" -Value @"
+    $f = New-Item -Path function: -Name "Global:Alias$Alias" -Options "AllScope" -Value @"
 Invoke-Expression '$cmd `$args '
     ###ALIAS###
 "@ 
     # Invoke-Expression '$cmd `$args'
 
-    Set-Alias -Name $Alias -Value "Alias$Alias" -Description "A UNIX-style alias using functions" -Option "AllScope" -scope Global -passThru 
+    $a = Set-Alias -Name $Alias -Value "Alias$Alias" -Description "A UNIX-style alias using functions" -Option "AllScope" -scope Global -passThru 
 }
 
 function unalias([string]$Alias,[switch]$Force){ 
