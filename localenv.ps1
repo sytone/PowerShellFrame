2dae8a93adecc21b900334ed8f813360c2c8b0a2
Sytone <github@sytone.com>
2016-01-19 16:19:40 -0800

    Lazy hack and commit

--- localenv.ps1
+++ localenv.ps1
@@ -396,5 +396,5 @@ if((Test-Path ".\localprofile.ps1")) {
     . .\localprofile.ps1
 }
 
-$tip = (cat .\tips.txt)[(Get-Random -Minimum 0 -Maximum ((cat .\tips.txt).Count + 1))]
+$tip = (cat Psf:\tips.txt)[(Get-Random -Minimum 0 -Maximum ((cat Psf:\tips.txt).Count + 1))]
 Write-Host "Tip: $tip" -ForegroundColor $Color_Label 
