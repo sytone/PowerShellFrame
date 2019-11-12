3c8fd2e9a03b6c6c615c085b7f27e801df9690b8
sytone <github@sytone.com>
2019-11-12 08:39:47 -0800

    Removed all application install logic. Added onedrive support for profile.

--- localenv.ps1
+++ localenv.ps1
@@ -83,35 +83,41 @@ function Show-SystemInfo {
   Write-Host "Code Signing Cert:".PadRight(24," ") -ForegroundColor $Color_Label -nonewline
   Write-Host $Cert.FriendlyName -ForegroundColor $Color_Value_2
 
-  Write-Host "Modules:".PadRight(24," ") -foregroundcolor $Color_Label -noNewLine
-  $StartingPosition = $Host.UI.RawUI.CursorPosition.X
-  Write-Host "".PadRight(30,"-") -ForegroundColor $Color_Label
-  
-  Get-Module | Format-Wide -AutoSize | Out-String -Width $( $HostWidth -$StartingPosition -1 ) -stream | Where-Object {$_} |  ForEach-Object{ Write-Host $($(" "*$StartingPosition) + $_) -foregroundColor $Color_Value_1}
-  
-  Write-Host "Functions:".PadRight(24," ") -foregroundcolor $Color_Label -noNewLine
-  $StartingPosition = $Host.UI.RawUI.CursorPosition.X
-  Write-Host "".PadRight(30,"-") -ForegroundColor $Color_Label
-  Get-ChildItem Scripts:CoreFunctions* -Recurse | Select-Object Name | Format-Wide -AutoSize | Out-String -Width $( $HostWidth -$StartingPosition -1 ) -stream | Where-Object {$_} |  ForEach-Object{ Write-Host $($(" "*$StartingPosition) + $($_.Replace(".ps1",""))) -foregroundColor $Color_Value_1}  
-  
-  $Host.UI.RawUI.ForegroundColor =$PreviousColor
-  Write-Host ""
-  Write-Host ""
+  if($env:PSFShowModules) {
+    Write-Host "Modules:".PadRight(24," ") -foregroundcolor $Color_Label -noNewLine
+    $StartingPosition = $Host.UI.RawUI.CursorPosition.X
+    Write-Host "".PadRight(30,"-") -ForegroundColor $Color_Label
+    
+    Get-Module | Format-Wide -AutoSize | Out-String -Width $( $HostWidth -$StartingPosition -1 ) -stream | Where-Object {$_} |  ForEach-Object{ Write-Host $($(" "*$StartingPosition) + $_) -foregroundColor $Color_Value_1}
+  }
 
-  $LogicalDisk = @()
-  Get-Volume | ForEach-Object {
-    $LogicalDisk += @($_ | Select-Object @{n="Name";e={$_.FriendlyName}},
-    @{n="Volume Label";e={$_.DriveLetter}},
-    @{n="Used (GB)";e={"{0:N2}" -f ( ($_.Size/1GB) - ($_.SizeRemaining/1GB) )}},
-    @{n="Free (GB)";e={"{0:N2}" -f ($_.SizeRemaining/1GB)}},
-    @{n="Size (GB)";e={"{0:N2}" -f ($_.Size/1GB)}},
-    @{n="Free (%)";e={if($_.Size) { "{0:N2}" -f ( ($_.SizeRemaining/1GB) / ($_.Size/1GB) * 100 )}else{"NAN"} }} )
-  } 
+  if($env:PSFShowFunctions) {
+    Write-Host "Functions:".PadRight(24," ") -foregroundcolor $Color_Label -noNewLine
+    $StartingPosition = $Host.UI.RawUI.CursorPosition.X
+    Write-Host "".PadRight(30,"-") -ForegroundColor $Color_Label
+    Get-ChildItem Scripts:CoreFunctions* -Recurse | Select-Object Name | Format-Wide -AutoSize | Out-String -Width $( $HostWidth -$StartingPosition -1 ) -stream | Where-Object {$_} |  ForEach-Object{ Write-Host $($(" "*$StartingPosition) + $($_.Replace(".ps1",""))) -foregroundColor $Color_Value_1}  
+    
+    $Host.UI.RawUI.ForegroundColor =$PreviousColor
+    Write-Host ""
+    Write-Host ""
+  }
 
-  Write-Host "Disks:".PadRight(24," ") -foregroundcolor $Color_Label  -noNewLine
-  $StartingPosition = $Host.UI.RawUI.CursorPosition.X
-  Write-Host "".PadRight(30,"-") -ForegroundColor $Color_Label
-  $LogicalDisk | format-table -AutoSize |  Out-String -Width $( $HostWidth -$StartingPosition -1 ) -stream | Where-Object {$_} |  ForEach-Object{ Write-Host $($(" "*$StartingPosition) + $_) -foregroundColor $Color_Value_1}  
+  if($env:PSFShowDrives) {
+    $LogicalDisk = @()
+    Get-Volume | ForEach-Object {
+      $LogicalDisk += @($_ | Select-Object @{n="Name";e={$_.FriendlyName}},
+      @{n="Volume Label";e={$_.DriveLetter}},
+      @{n="Used (GB)";e={"{0:N2}" -f ( ($_.Size/1GB) - ($_.SizeRemaining/1GB) )}},
+      @{n="Free (GB)";e={"{0:N2}" -f ($_.SizeRemaining/1GB)}},
+      @{n="Size (GB)";e={"{0:N2}" -f ($_.Size/1GB)}},
+      @{n="Free (%)";e={if($_.Size) { "{0:N2}" -f ( ($_.SizeRemaining/1GB) / ($_.Size/1GB) * 100 )}else{"NAN"} }} )
+    } 
+
+    Write-Host "Disks:".PadRight(24," ") -foregroundcolor $Color_Label  -noNewLine
+    $StartingPosition = $Host.UI.RawUI.CursorPosition.X
+    Write-Host "".PadRight(30,"-") -ForegroundColor $Color_Label
+    $LogicalDisk | format-table -AutoSize |  Out-String -Width $( $HostWidth -$StartingPosition -1 ) -stream | Where-Object {$_} |  ForEach-Object{ Write-Host $($(" "*$StartingPosition) + $_) -foregroundColor $Color_Value_1}  
+  }
 
   Write-Host "Uptime:".PadRight(24," ") -foregroundcolor $Color_Label -noNewLine
   $StartingPosition = $Host.UI.RawUI.CursorPosition.X
@@ -145,26 +151,6 @@ function Show-SystemInfo {
   }
 
 }
-
-
-#
-# NEed to workout where tools and dev items go...
-#
-function Install-Tools {
-  if(-not $Global:IsAdmin) {
-    Write-Host "Restart console as admin. [sudo powershell]"
-    return
-  }
-  
-  if($env:path -match "Chocolatey") {
-    Write-Host "Chocolatey already installed."
-  } else {
-    Write-Host "Installing Chocolatey..."
-    Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-  }
-}
-
-
 
 # Create configuration if missing with defaults.
 if (-not (Test-Path Psf:\config.xml)) {
@@ -211,94 +197,16 @@ function Set-LocationTools {
   Set-LocationWithPathCheck (Get-PsfConfig -Key ToolsPath)
 }
 
-function Start-Cmder {
-  $path = Join-Path (Get-PsfConfig -Key ToolsPath) "cmder"
-  $TargetFile = (Join-Path $path 'cmder.exe')
-  if((Test-Path $TargetFile)) {
-    Start-Process $TargetFile
-  } else {
-    Install-Cmder
-  }
-}
-
 set-alias cdev Set-LocationDevelopment
 set-alias ctools Set-LocationTools;
 set-alias sudo Start-ElevatedProcess;
 set-alias reload Restart-Host;
 set-alias updatepsf Update-PSF;
-set-alias cmder Start-Cmder;
-
-switch ( $Host.Name ) {
-    'Windows PowerShell ISE Host' {
-        $Global:Color_Label = $Global:PsfConfiguration.IseColorLabel
-        $Global:Color_Value_1 = $Global:PsfConfiguration.IseColorValue1
-        $Global:Color_Value_2 = $Global:PsfConfiguration.IseColorValue2
-        $HostWidth = 80
-
-        Import-Module ISEPack 
-
-        $PSISE.options.FontName = $Global:PsfConfiguration.IseFontName
-        $psise.Options.ConsolePaneBackgroundColor = $Global:PsfConfiguration.IseBackgroundColor
-        $psise.Options.ConsolePaneTextBackgroundColor = $Global:PsfConfiguration.IseTextBackgroundColor
-        $psise.Options.ConsolePaneForegroundColor = $Global:PsfConfiguration.IseForegroundColor
-
-        # watch for changes to the Files collection of the current Tab
-        register-objectevent $psise.CurrentPowerShellTab.Files collectionchanged -action {
-          # iterate ISEFile objects
-          $event.sender | ForEach-Object {
-            # set private field which holds default encoding to ASCII
-            $_.gettype().getfield("encoding","nonpublic,instance").setvalue($_, [text.encoding]::ascii)
-          }
-        } | Out-null
-    }
-    default
-    {
-      $pshost = get-host
-      $global:ConsoleHost = ($pshost.Name -eq "ConsoleHost")
-      $pswindow = $pshost.ui.rawui
-      
-      #Setup console colours I like!
-      #$pswindow.ForegroundColor = "Green"
-      #Write-Host $pswindow.BackgroundColor
-      #$pswindow.BackgroundColor = "Black"
-      if($pswindow.buffersize.height -le $Global:PsfConfiguration.WindowHeightBuffer -and $pswindow.buffersize.width -le $Global:PsfConfiguration.WindowWidth ) {
-            trap {Continue}
-            $newsize = $pswindow.buffersize
-            $newsize.height = $Global:PsfConfiguration.WindowHeightBuffer
-            $newsize.width = $Global:PsfConfiguration.WindowWidth
-            $pswindow.buffersize = $newsize
-      }
-      
-      if($pswindow.windowsize.height -le $Global:PsfConfiguration.WindowHeight -and $pswindow.windowsize.width -le $Global:PsfConfiguration.WindowWidth ) {
-            trap {Continue}
-            $newsize = $pswindow.windowsize
-            $newsize.height = $Global:PsfConfiguration.WindowHeight
-            $newsize.width = $Global:PsfConfiguration.WindowWidth
-            $pswindow.windowsize = $newsize            
-      }
-
-      $Global:Color_Label = $Global:PsfConfiguration.HostColorLabel
-      $Global:Color_Value_1 = $Global:PsfConfiguration.HostColorValue1
-      $Global:Color_Value_2 = $Global:PsfConfiguration.HostColorValue2
-      $HostWidth = $Host.UI.RawUI.WindowSize.Width
-      
-      # Set a nice title for the window. 
-      if(!$global:WindowTitlePrefix) {
-        if ($IsAdmin) {
-          $global:WindowTitlePrefix = "PowerShell (ADMIN) - "
-        } else {
-          $global:WindowTitlePrefix = "PowerShell - "
-        }
-      }
-      
-      $pswindow.WindowTitle = "$($global:WindowTitlePrefix) [Local Environment]"
-    }
-}
 
 #
-# Add variable for onedrive from registry if installed and mapping drive.  
+# Add variable for onedrive from registry if installed and mapping drive. Personal is the default.
 #
-$onedriveProperty = Get-ItemProperty -Path "hkcu:\Software\Microsoft\OneDrive\" -Name UserFolder -ErrorAction SilentlyContinue
+$onedriveProperty = Get-ItemProperty -Path "hkcu:\Software\Microsoft\OneDrive\Personal" -Name UserFolder -ErrorAction SilentlyContinue
 if($onedriveProperty) {
   $onedrive = $onedriveProperty.UserFolder
 }
@@ -319,89 +227,31 @@ if($onedrive) {
 Get-Item Scripts:CoreFunctions | Where-Object { $_.PsIsContainer } | ForEach-Object {Add-DirectoryToPath -Directory "$($_.FullName)" }
 Get-ChildItem Scripts:CoreFunctions* | Where-Object { $_.PsIsContainer } | ForEach-Object {Add-DirectoryToPath -Directory "$($_.FullName)" }
 
-# Setup the prompt
-function Global:prompt {
-  $realLASTEXITCODE = $LASTEXITCODE
-  # The at sign creates an array in case only one history item exists.
-  $history = @(get-history)
-  if($history.Count -gt 0) {
-    $lastItem = $history[$history.Count - 1]
-    $lastId = $lastItem.Id
-  }
-  $nextCommand = $lastId + 1
-  Write-Host "PS: $nextCommand $($executionContext.SessionState.Path.CurrentLocation)" -ForegroundColor Gray
-  if(Get-Module -Name posh-git) {
-    Write-VcsStatus
-  }
-  "$('#' * ($nestedPromptLevel + 1)) "
-
-
-  $global:LASTEXITCODE = $realLASTEXITCODE  
-}
-
 Show-SystemInfo
 
-# Load the local profile. If found then load it. 
+# Load the local profile. If found then load it.
 if((Test-Path ".\localprofile.ps1")) {
     . .\localprofile.ps1
 }
 
-# Load machine specific profie. this is not backed up using One Drive. 
+# Load the onedrive profile. If found then load it.
+if($onedrive) {
+  if((Test-Path "$onedrive\scripts\powershell\localprofile.ps1")) {
+    . "$onedrive\scripts\powershell\localprofile.ps1"
+  }  
+}
+
+# Load machine specific profie.
 if((Test-Path ".\localprofile.$($env:COMPUTERNAME).ps1")) {
     . ".\localprofile.$($env:COMPUTERNAME).ps1"
 }
 
-function Install-Cmder {
-  $miniInstallSource = 'https://github.com/cmderdev/cmder/releases/download/v1.3.5/cmder_mini.zip'
-  $fullInstallSource = 'https://github.com/cmderdev/cmder/releases/download/v1.3.5/cmder.zip'
-  $enableCmder = [bool](Read-Choice "Do you want to enable the cmder - http://cmder.net/ ?" "&No","&Yes" -Default 1)
-  if($enableCmder) {
-    $installFull = [bool](Read-Choice "Do you want to install the Full or Mini version?" "&Mini","&Full" -Default 1)
-    Write-Host "Installing CMDER to tools. Please wait..."
-    
-    $path = Join-Path (Get-PsfConfig -Key ToolsPath) "cmder"
-    if(!(Test-Path $path)) {New-Item -Path $path -ItemType Directory | Out-Null }
-    
-    $currentSecurityProtocol = [Net.ServicePointManager]::SecurityProtocol
-    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-    if($installFull) {
-      Invoke-WebRequest -Uri $miniInstallSource -OutFile (Join-Path $path 'cmder.zip')
-    } else {
-      Invoke-WebRequest -Uri $miniInstallSource -OutFile (Join-Path $path 'cmder.zip')
-    }
-    [Net.ServicePointManager]::SecurityProtocol = $currentSecurityProtocol
-    
-    Expand-Archive -Path (Join-Path $path 'cmder.zip') -DestinationPath $path
-    $TargetFile = (Join-Path $path 'cmder.exe')
-    $ShortcutFile = Join-Path (Get-PsfConfig -Key ToolsPath) "cmder.cmd"
-    "@ECHO OFF`n$TargetFile" | Out-File -FilePath $ShortcutFile -Force -Encoding ascii
-    
-    # Update the profile so PSF loads in CMDER
-    $cmderProfile = Join-Path (Get-PsfConfig -Key ToolsPath) "cmder\config\user-profile.ps1"
-    $psfLocalRoot =  Join-Path $env:USERPROFILE "psf"
-    $envLoadLine = "`n. $psfLocalRoot\localenv.ps1   #LOCALENV - May change in future`n"
-    New-Item $cmderProfile -type file -force -ea 0 | Out-Null
-    $envLoadLine | Set-Content  ($cmderProfile)
-    
-    # Remove old PSGet
-    $psgetPath = Join-Path (Join-Path (Join-Path (Join-Path (Get-PsfConfig -Key ToolsPath) "cmder") "vendor") "psmodules") "PsGet"
-    Remove-Item $psgetPath -Recurse -Force | Out-Null
-    Set-PsfConfig -Key 'CMDER_ENABLED' -Value 'enabled'
+# Load the onedrive machine specific profile. If found then load it.
+if($onedrive) {
+  if((Test-Path "$onedrive\scripts\powershell\localprofile.$($env:COMPUTERNAME).ps1")) {
+    . "$onedrive\scripts\powershell\localprofile.$($env:COMPUTERNAME).ps1"
+  }  
 }
-
-#Install cmder?
-if((Get-PsfConfig -Key 'CMDER_ENABLED') -eq 'unknown' -or (Get-PsfConfig -Key 'CMDER_ENABLED') -eq $null) {
-    Install-Cmder
-  } else {
-    Set-PsfConfig -Key 'CMDER_ENABLED' -Value 'disabled'
-  }
-}
-
-#TODO
-# Visual Studio Code check and Install
-#  choco install VisualStudioCode -y
-# GIT check and install 
-#  choco install git -y
 
 # Friendly Tips!
 $tip = (Get-Content psf:\tips.txt)[(Get-Random -Minimum 0 -Maximum ((Get-Content psf:\tips.txt).Count + 1))]
@@ -414,6 +264,8 @@ Write-Host " $tip `n`n"
   [Net.SecurityProtocolType]::Tls11 -bor `
   [Net.SecurityProtocolType]::Tls
 
+# If the retain path variable is set or in vs code pop back to the launch location. Otherwise
+# stay in the profile directory.
 if($env:psfretainpath -eq 'true' -or $env:TERM_PROGRAM -eq 'vscode') {
   Pop-Location
 }
